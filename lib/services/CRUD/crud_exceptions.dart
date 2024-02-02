781a1edbeccfd5c1a1cd54b6018a9e57afa58e68
HP <ksaud3508@gmail.com>
2024-02-02 22:46:13 +0500

    step 7

---
+++ lib/services/CRUD/crud_exceptions.dart
@@ -0,0 +1,18 @@
+
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CloudNotDeleteUser implements Exception{}
+
+class UserAlreadyExists implements Exception{}
+
+class CouldNotFindUser implements Exception{}
+
+class  CouldNotDeleteNote implements Exception{}
+
+class CouldNotFindNotes implements Exception{}
+
+class CouldNotUpdateNotes implements Exception{}
